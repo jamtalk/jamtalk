59b88f948215cfe3a679d276994cd242dd17a432
GameJE <xx9582@naver.com>
2022-06-14 15:08:49 +0900

    랜덤 쉐이커 이펙트 추가 112번 컨텐츠 랜덤쉐이커 이펙트 적용

---
+++ Assets/Scripts/Animation/AnimationScript/RandomShaker.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class RandomShaker : AnimationScript
+{
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float randomRange;
+    [SerializeField]
+    [Range(1f, 90f)]
+    private float angle;
+    Coroutine shaking;
+    public override void Play()
+    {
+        StartCoroutine(Shaking());
+    }
+
+    public override void Stop()
+    {
+        if (shaking != null)
+            StopCoroutine(shaking);
+    }
+    IEnumerator Shaking()
+    {
+        while (true)
+        {
+            var delay = Random.Range(0f, randomRange);
+            yield return new WaitForSecondsRealtime(delay);
+            if (!gameObject.activeSelf)
+                continue;
+            var seq = DOTween.Sequence();
+            var startTween = transform.DORotate(new Vector3(0, 0, angle),duration/4);
+            startTween.SetEase(Ease.Linear);
+            startTween.SetLoops(2, LoopType.Yoyo);
+
+            var endTween = transform.DORotate(new Vector3(0, 0, -angle), duration / 4);
+            endTween.SetEase(Ease.Linear);
+            endTween.SetLoops(2, LoopType.Yoyo);
+            seq.Append(endTween);
+
+            bool isSeq = false;
+            seq.onComplete += () => isSeq = true;
+            seq.Play();
+            while (!isSeq) { yield return null; }
+
+        }
+    }
+}
