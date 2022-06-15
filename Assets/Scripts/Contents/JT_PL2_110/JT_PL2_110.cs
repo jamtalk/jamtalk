bbb9d9d2c877ed02a17b61758b5f50dfd1136282
yungwon <kwony1537@naver.com>
2022-06-15 18:04:06 +0900

    2단계 110번 UI

---
+++ Assets/Scripts/Contents/JT_PL2_110/JT_PL2_110.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class JT_PL2_110 : JT_PL1_117
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    
+}
