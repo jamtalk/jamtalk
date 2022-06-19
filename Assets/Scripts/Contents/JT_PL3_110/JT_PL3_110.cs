e6ec64077cdfc2d7d80ee78598ba2df0c2371786
yungwon <kwony1537@naver.com>
2022-06-19 18:49:09 +0900

    3단계 110/ 4단계 110 구현

---
+++ Assets/Scripts/Contents/JT_PL3_110/JT_PL3_110.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JT_PL3_110 : BingoContents<WordsData.WordSources, WordBingoButton, Text, WordBingoBoard>
+{
+    protected override eContents contents => eContents.JT_PL3_110;
+
+    protected override WordsData.WordSources[] correctsTarget =>
+        new eAlphabet[] { GameManager.Instance.currentAlphabet, GameManager.Instance.currentAlphabet + 1 }
+        .SelectMany(x => GameManager.Instance.GetResources(x).Words)
+        .Where(x => x.value.Length < 6)
+        .ToArray();
+
+    public override WordsData.WordSources[] GetQuestionType()
+    {
+        return GameManager.Instance.alphabets
+            .Select(x => GameManager.Instance.GetResources(x))
+            .Where(x => !correctsTarget.Select(y => y.alphabet).Contains(x.Alphabet))
+            .SelectMany(x => x.Words)
+            .Where(x => x.value.Length < 6)
+            .Take((int)Mathf.Pow(board.size, 2f))
+            .OrderBy(x => Random.Range(0f, 100f))
+            .ToArray();
+    }
+
+    protected override bool IsCurrentAnswer(WordsData.WordSources value) => value == currentQuestion;
+
+    protected override AudioClip GetClip() => currentQuestion.clip;
+}
