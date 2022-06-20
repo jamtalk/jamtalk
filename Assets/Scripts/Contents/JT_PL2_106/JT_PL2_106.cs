6130aff87ab3735b52892c4756c6a5a7c07beea0
yungwon <kwony1537@naver.com>
2022-06-20 11:40:15 +0900

    2단계 106번 구현

--- Assets/Scripts/Contents/JT_PL2_106/JT_PL2_106.cs
+++ Assets/Scripts/Contents/JT_PL2_106/JT_PL2_106.cs
@@ -6,68 +6,145 @@ using System.Linq;
 using DG.Tweening;
 using UnityEngine.EventSystems;
 
-public class JT_PL2_106 : MultiAnswerContents<Question2_106, WordsData.WordSources>
+public class JT_PL2_106 : BaseContents
 {
     public EventSystem eventSystem;
     protected override eContents contents => eContents.JT_PL2_106;
-    protected override bool CheckOver() => currentQuestionIndex == questions.Count - 1;
+    protected override bool CheckOver() => QuestionCount == index;
     protected override int GetTotalScore() => QuestionCount;
-    protected override int QuestionCount => 5;
+    protected int QuestionCount => 5;
     protected virtual int WordsCount => 6;
+    private int index = 0;
 
     public Button longButton;
     public Button shortButton;
     public Button spinButton;
-    public Image[] images;
-    public Sprite succesImage;
+    public Toggle[] currentCount;
     public Image rouletteImage;
     public GameObject parent;
     public Text rouletteText;
+    public Image rouletteEffect;
 
+    public AudioSinglePlayer audioPlayer;
     public AudioClip rouletteClip;
     public AudioClip tabClip;
     public AudioClip currentClip;
+
+    private WordsData.WordSources[] shortWords;
+    private WordsData.WordSources[] longWords;
 
     private Sequence seq;
     private List<Text> textList = new List<Text>();
     private int currentIndex;
     private List<WordsData.WordSources> datas = new List<WordsData.WordSources>();
-    private Question2_106 Question2;
 
     protected override void Awake()
-    {
+    {          
         base.Awake();
-
+        rouletteEffect.gameObject.SetActive(false);
+        Init();
         spinButton.onClick.AddListener(() => Spin());
-
+        shortButton.onClick.AddListener(() => ButtonListener(shortButton));
+        longButton.onClick.AddListener(() => ButtonListener(longButton));
     }
-    private void AddOnClickButtonListener(WordsData.WordSources data)
-    {
-        shortButton.onClick.AddListener(() =>
-        {
-            audioPlayer.Play(tabClip);
 
-            if (currentQuestion.currentCorrect == datas[currentIndex])
-            {
-                images[currentQuestionIndex].sprite = succesImage;
-                AddAnswer(data);
-            }
-        });
-        longButton.onClick.AddListener(() =>
+    private void Init()
+    {
+        shortWords = GameManager.Instance.GetResources().Words
+            .OrderBy(x => Random.Range(0f, 100f))
+            .Take(WordsCount)
+            .ToArray();
+
+        longWords = GameManager.Instance.alphabets
+            .Where(x => x != GameManager.Instance.currentAlphabet)
+            .SelectMany(x => GameManager.Instance.GetResources(x).Words)
+            .OrderBy(x => Random.Range(0f, 100f))
+            .Take(WordsCount)
+            .ToArray();
+
+        rouletteText.gameObject.SetActive(true);
+
+        for (int i = 0; i < shortWords.Length; i++)
         {
-            audioPlayer.Play(tabClip);
-        });
+            var angle = 15f + (float)(360 / (WordsCount * 2)) * i;
+
+            var text = Instantiate(rouletteText.gameObject, parent.transform).GetComponent<Text>();
+            text.transform.localRotation = Quaternion.Euler(0, 0, angle);
+            text.text = shortWords[i].value;
+            text.name = shortWords[i].value;
+
+            textList.Add(text);
+            datas.Add(shortWords[i]);
+        }
+
+        for (int i = 0; i < longWords.Length; i++)
+        {
+            var angle = 15f + (float)(360 / (WordsCount * 2)) * (WordsCount + i);
+
+            var text = Instantiate(rouletteText.gameObject, parent.transform).GetComponent<Text>();
+            text.transform.localRotation = Quaternion.Euler(0, 0, angle);
+            text.text = longWords[i].value;
+            text.name = longWords[i].value;
+
+            textList.Add(text);
+            datas.Add(longWords[i]);
+        }
+
+        rouletteText.gameObject.SetActive(false);
+    }
+
+    private void ButtonListener(Button button)
+    {
+        for(int i = 0; i < WordsCount; i++)
+        {
+            if (button.name == "ShortButton")
+            {
+                if (textList[currentIndex].text.Contains(shortWords[i].value))
+                {
+                    currentCount[index].isOn = true;
+                    index += 1;
+                    audioPlayer.Play(1f, GameManager.Instance.GetClipCorrectEffect(), () =>
+                    {
+                        if (CheckOver())
+                            ShowResult();
+                    });
+                    shortButton.interactable = false;
+                    longButton.interactable = false;
+                    break;
+                }
+            }
+            else if (button.name == "LongButton")
+            {
+                if (textList[currentIndex].text.Contains(longWords[i].value))
+                {
+                    currentCount[index].isOn = true;
+                    index += 1;
+                    audioPlayer.Play(1f, GameManager.Instance.GetClipCorrectEffect(), () =>
+                    {
+                        if (CheckOver())
+                            ShowResult();
+                    });
+                    shortButton.interactable = false;
+                    longButton.interactable = false;
+                    break;
+                }
+            }
+        }
     }
 
     private void Spin()
-    {   // 시작 시 eventSystemp false > onCompleted true
+    {
+        rouletteEffect.gameObject.SetActive(true);
+        shortButton.interactable = true;
+        longButton.interactable = true;
+
         if (seq != null)
         {
             seq.Kill();
             seq = null;
         }
         audioPlayer.Play(6f, rouletteClip);
-        //eventSystem.enabled = false;
+        eventSystem.enabled = false;
 
         seq = DOTween.Sequence();
         currentIndex = Random.Range(0, textList.Count);
@@ -85,72 +162,11 @@ public class JT_PL2_106 : MultiAnswerContents<Question2_106, WordsData.WordSourc
         seq.Append(secondTween);
         seq.Append(lastTween);
 
-        //seq.onComplete += () => eventSystem.enabled = true;
+        seq.onComplete += () =>
+        {
+            eventSystem.enabled = true;
+            rouletteEffect.gameObject.SetActive(false);
+        };
         seq.Play();
     }
-
-    protected override void ShowQuestion(Question2_106 question)
-    {
-        rouletteText.gameObject.SetActive(true);
-
-        for (int i = 0; i < question.totalQuestion.Length; i++)
-        {
-            var angle = 15f + (float)(360 / question.totalQuestion.Length) * i;
-
-            var text = Instantiate(rouletteText.gameObject, parent.transform).GetComponent<Text>();
-            text.transform.localRotation = Quaternion.Euler(0, 0, angle);
-            text.text = question.totalQuestion[i].value;
-            text.name = question.totalQuestion[i].value;
-
-            textList.Add(text);
-            datas.Add(question.totalQuestion[i]);
-            AddOnClickButtonListener(question.totalQuestion[i]);
-        }
-        rouletteText.gameObject.SetActive(false);
-    }
-
-    protected override List<Question2_106> MakeQuestion()
-    {
-        var list = new List<Question2_106>();
-        for(int i = 0; i < QuestionCount; i++)
-        {
-            var shortWords = GameManager.Instance.GetResources().Words
-                .OrderBy(x => Random.Range(0f, 100f))
-                .Take(WordsCount)
-                .ToArray();
-
-            var longWords = GameManager.Instance.alphabets
-                .Where(x => x != GameManager.Instance.currentAlphabet)
-                .SelectMany(x => GameManager.Instance.GetResources(x).Words)
-                .OrderBy(x => Random.Range(0f, 100f))
-                .Take(WordsCount)
-                .ToArray();
-            list.Add(new Question2_106(shortWords, longWords));
-        }
-        return list;
-    }
-}
-
-
-
-public class Question2_106 : MultiQuestion<WordsData.WordSources>
-{
-    public int currentIndex;
-    public Question2_106(WordsData.WordSources[] correct, WordsData.WordSources[] questions) : base(correct, questions)
-    {
-        currentIndex = 0;
-    }
-    public WordsData.WordSources currentCorrect => correct[currentIndex];
-
-    protected override bool CheckCorrect(WordsData.WordSources answer)
-    {
-        return correct[currentIndex] == answer;
-    }
-
-    public override void SetAnswer(WordsData.WordSources answer)
-    {
-        base.SetAnswer(answer);
-        currentIndex += 1;
-    }
-
 }
