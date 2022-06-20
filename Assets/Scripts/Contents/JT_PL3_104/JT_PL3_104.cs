460abd8431abb907ce75580d3ac033f0c26eae93
yungwon <kwony1537@naver.com>
2022-06-20 18:54:11 +0900

    3단계 104번 구현

---
+++ Assets/Scripts/Contents/JT_PL3_104/JT_PL3_104.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JT_PL3_104 : JT_PL2_104
+{
+    protected override eContents contents => eContents.JT_PL3_104;
+    public GameObject effect;
+    public AudioClip effectClip;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override List<Question2_104> MakeQuestion()
+    {
+        return base.MakeQuestion();
+    }
+
+    protected override void ThrowElement(BubbleElement bubble, WordsData.WordSources data)
+    {
+        thrower.Throw(bubble, textPot.GetComponent<RectTransform>(), () =>
+        {
+            effect.gameObject.SetActive(true);
+            audioPlayer.Play(1f, effectClip, () =>
+            {
+                AddAnswer(data);
+                effect.gameObject.SetActive(false);
+            });
+        });
+    }
+}
