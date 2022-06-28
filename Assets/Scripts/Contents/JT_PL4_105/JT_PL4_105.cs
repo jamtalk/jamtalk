fb563372c2831c25ddab7e6f1a2a89088e401741
yungwon <kwony1537@naver.com>
2022-06-28 18:03:45 +0900

    4단계 105 1차 구현

---
+++ Assets/Scripts/Contents/JT_PL4_105/JT_PL4_105.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JT_PL4_105 : BaseContents
+{
+    protected override eContents contents => eContents.JT_PL4_105;
+    protected override bool CheckOver() => questionCount == index;
+    protected override int GetTotalScore() => questionCount;
+    private int questionCount = 3;
+    private int index = 0;
+    private DigraphsSource current;
+
+    public Image currentImage;
+    public RectTransform wordLayout;
+    public GameObject wordElement;
+    public GameObject digraphsElement;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        MakeQuestion();
+    }
+
+    private void MakeQuestion()
+    {
+        current = GameManager.Instance.digrpahs
+            .SelectMany(x => GameManager.Instance.GetDigraphs(x))
+            .Where(x => x.type == GameManager.Instance.currentDigrpahs)
+            .OrderBy(x => Random.Range(0f, 100f))
+            .First();
+
+        ShowQuestion();
+    }
+
+    private void ShowQuestion()
+    {
+        currentImage.sprite = current.sprite;
+        currentImage.preserveAspect = true;
+
+        var digraphs = current.type.ToString().ToLower();
+        var currentTemp = current.value.Replace(digraphs, string.Empty);
+        var tempList = new List<string>();
+        foreach (var item in currentTemp)
+            tempList.Add(item.ToString());
+        tempList.Add(digraphs);
+
+        for(int i = 0; i < tempList.Count; i++)
+        {
+            if (current.type.ToString() == tempList[i].ToUpper())
+            {
+                var textElemet = Instantiate(digraphsElement, wordLayout).GetComponent<wordElement405>();
+                textElemet.Init(current.type);  // oi oy 등으로 변경되게 수정 필요 
+                DigraphsButtonAddListener(textElemet.currentButton, textElemet.worngButton);
+            }
+            else
+            {
+                var textElemet = Instantiate(wordElement, wordLayout).GetComponent<wordElement405>();
+                textElemet.Init(tempList[i]);
+            }
+        }
+
+
+    }
+
+    private void DigraphsButtonAddListener(Button currentButton, Button worngButton)
+    {
+        currentButton.onClick.AddListener(() =>
+        {
+            
+        });
+
+        worngButton.onClick.AddListener(() =>
+        {
+
+        });
+    }
+}
---
+++ Assets/Scripts/Contents/JT_PL4_105/wordElement405.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wordElement405 : MonoBehaviour
+{
+    public Text text;
+    public Text texts;
+    public Button currentButton;
+    public Button worngButton;
+
+    public void Init(string value)
+    {
+        text.text = value;
+    }
+    public void Init(eDigraphs value)
+    { 
+        text.text = value.ToString().ToLower();
+        texts.text = value.ToString();
+    }
+}
