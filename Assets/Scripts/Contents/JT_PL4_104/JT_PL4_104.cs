511b79d11c7d49149b7966625ae3e8ded805cdb8
yungwon <kwony1537@naver.com>
2022-06-28 18:30:44 +0900

    4단계 104번 1차구현

---
+++ Assets/Scripts/Contents/JT_PL4_104/JT_PL4_104.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JT_PL4_104 : BaseContents
+{
+    protected override eContents contents => eContents.JT_PL4_104;
+    protected override bool CheckOver() => questionCount == index;
+    protected override int GetTotalScore() => questionCount;
+    private int questionCount = 3;
+    private int index = 0;
+
+    public WordElement404[] elements;
+
+    private eDigraphs[] eDig = { eDigraphs.OI, eDigraphs.AI, eDigraphs.EA
+                                ,eDigraphs.OW, eDigraphs.AW, eDigraphs.IE};
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        MakeQuestion();
+    }
+
+
+    private void MakeQuestion()
+    {
+        var data = eDig.OrderBy(x => Random.Range(0f, 100f)).ToArray();
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            elements[i].Init(data[i]);
+            AddListener(elements[i]);
+        }
+    }
+
+    private void AddListener(WordElement404 element)
+    {
+        var button = element.button;
+
+        button.onClick.AddListener(() =>
+        {
+            // 해당 음가 출력
+            element.Open();    
+        });
+    }
+}
+
---
+++ Assets/Scripts/Contents/JT_PL4_104/WordElement404.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordElement404 : MonoBehaviour
+{
+    public Text text;
+    public Image closeImage;
+    public Image openImage;
+    public GameObject charactor;
+    public Button button;
+
+    public void Init(eDigraphs value)
+    {
+        text.text = value.ToString().ToLower();
+    }
+    public void Open()
+    {
+        closeImage.gameObject.SetActive(false);
+        openImage.gameObject.SetActive(true);
+    }
+    public void Correct()
+    {
+        charactor.gameObject.SetActive(false);
+    }
+}
