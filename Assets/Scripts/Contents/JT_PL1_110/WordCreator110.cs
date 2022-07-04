a002cb7771d2aee28b0edce121fe98ca9ba60a27
yungwon <kwony1537@naver.com>
2022-07-04 19:17:27 +0900

    3단계 109번 누락본

--- Assets/Scripts/Contents/JT_PL1_110/WordCreator110.cs
+++ Assets/Scripts/Contents/JT_PL1_110/WordCreator110.cs
@@ -21,4 +21,18 @@ public class WordCreator110 : MonoBehaviour
         }
         return elements.ToArray();
     }
+
+    public AlphabetToggle110[] Create(DigraphsSource data)
+    {
+        var word = data.value.Replace(" ", "");
+        var elements = new List<AlphabetToggle110>();
+        for (int i = 0; i < word.Length; i++)
+        {
+            var alphabet = (eAlphabet)System.Enum.Parse(typeof(eAlphabet), word[i].ToString().ToUpper());
+            var component = Instantiate(element, parent).GetComponent<AlphabetToggle110>();
+            component.Init(alphabet);
+            elements.Add(component);
+        }
+        return elements.ToArray();
+    }
 }
