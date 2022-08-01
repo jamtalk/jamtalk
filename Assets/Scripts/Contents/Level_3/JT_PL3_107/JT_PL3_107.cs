cb05fce28cd1393b407e223358fa70b0ab87bad5
yungwon <kwony1537@naver.com>
2022-08-01 14:30:18 +0900

    3단계 107번 수정

--- Assets/Scripts/Contents/Level_3/JT_PL3_107/JT_PL3_107.cs
+++ Assets/Scripts/Contents/Level_3/JT_PL3_107/JT_PL3_107.cs
@@ -7,7 +7,7 @@ public class JT_PL3_107 : BaseMatchImage<DigraphsWordsData>
 {
     protected override eContents contents => eContents.JT_PL3_107;
     protected override int GetTotalScore() => drops.Length;
-    protected override bool CheckOver() => digraphsIndex == eDig.Length;
+    //protected override bool CheckOver() => digraphsIndex == eDig.Length;
 
     protected eDigraphs[] eDig = { eDigraphs.CH, eDigraphs.SH, eDigraphs.TH };
     protected int digraphsIndex = 0;
@@ -17,36 +17,36 @@ public class JT_PL3_107 : BaseMatchImage<DigraphsWordsData>
     {
         words = GameManager.Instance.digrpahs
             .SelectMany(x=>GameManager.Instance.GetDigraphs(x))
-            .Where( x => x.digraphs == eDig[digraphsIndex].ToString())
+            .Where( x => x.Digraphs == GameManager.Instance.currentDigrpahs)
             .OrderBy(x => Random.Range(0f, 100f))
             .Take(drops.Length)
             .ToArray();
         SetElement(words);
     }
-    protected override void onDrop()
-    {
-        dropCount += 1;
+    //protected override void onDrop()
+    //{
+    //    dropCount += 1;
 
-        if (dropCount == words.Length)
-        { 
-            digraphsIndex += 1;
-            if (eDig.Length == digraphsIndex)
-                base.onDrop();
-            else
-            {
-                GetWords();
-                for (int i = 0; i < words.Length; i++)
-                {
-                    drags[i].Reset();
-                    drops[i].Reset();
-                }
+    //    if (dropCount == words.Length)
+    //    { 
+    //        digraphsIndex += 1;
+    //        if (eDig.Length == digraphsIndex)
+    //            base.onDrop();
+    //        else
+    //        {
+    //            GetWords();
+    //            for (int i = 0; i < words.Length; i++)
+    //            {
+    //                drags[i].Reset();
+    //                drops[i].Reset();
+    //            }
 
-                dropCount = 0;
+    //            dropCount = 0;
 
-            }
-        }
-        audioPlayer.Play(1f, GameManager.Instance.GetClipCorrectEffect());
-    }
+    //        }
+    //    }
+    //    audioPlayer.Play(1f, GameManager.Instance.GetClipCorrectEffect());
+    //}
 
     protected override void PlayAudio(ResourceWordsElement word)
     {
