b6c584b30ede9b55059dac00a8a891a0eff361c8
yungwon <kwony1537@naver.com>
2022-10-11 18:13:18 +0900

    1단계 116번 Guide 구현

--- Assets/Scripts/Contents/Level_1/JT_PL1_116/JT_PL1_116.cs
+++ Assets/Scripts/Contents/Level_1/JT_PL1_116/JT_PL1_116.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class JT_PL1_116 : BaseContents
 {
@@ -25,12 +27,51 @@ public class JT_PL1_116 : BaseContents
 
     private List<AlphabetButton> selected = new List<AlphabetButton>();
     protected override int GetTotalScore() => upper.Length;
+    bool isNext = false;
+    protected override IEnumerator ShowGuidnceRoutine()
+    {
+        yield return base.ShowGuidnceRoutine();
 
+
+        for(int i = 0; i < length; i++)
+        {
+            isNext = false;
+
+            var guideUpper = upper.Where(x => x.button.interactable)
+                .Where(x => x.type == eAlphabetType.Upper)
+                .Where(x => x.value == words[currentIndex].Key)
+                .First();
+            var guideLower = lower.Where(x => x.button.interactable)
+                .Where(x => x.type == eAlphabetType.Lower)
+                .Where(x => x.value == guideUpper.value)
+                .First();
+
+
+            AlphabetButton[] guideButtons = { guideUpper, guideLower };
+            guideButtons = guideButtons.OrderBy(x => Random.Range(0, 100)).ToArray();
+
+            foreach (var item in guideButtons)
+            {
+                guideFinger.gameObject.SetActive(true);
+
+                guideFinger.DoMoveCorrect(item.transform.position, () =>
+                {
+                    guideFinger.DoClick(() =>
+                    {
+                        ButtonClickMotion(item);
+                    });
+                });
+
+                while (!isNext) yield return null;
+                isNext = false;
+            }
+        }
+    }
     protected override void Awake()
     {
         base.Awake();
         for (int i = 0; i < buttonPlayer.Length; i++)
-            buttonPlayer[i].onClick.AddListener(PlayWord);
+            buttonPlayer[i].onClick.AddListener(() => PlayWord());
         var current = new eAlphabet[] { GameManager.Instance.currentAlphabet, GameManager.Instance.currentAlphabet + 1 };
         if (GameManager.Instance.currentAlphabet < eAlphabet.C)
             alphabets = current.SelectMany(x => new eAlphabet[] { x, x }).ToArray();
@@ -67,51 +108,70 @@ public class JT_PL1_116 : BaseContents
         }
         PlayWord();
     }
-    private void PlayWord()
+    private void PlayWord(Action action = null)
     {
-        audioPlayer.Play(words[currentIndex].audio.act2);
+        audioPlayer.Play(words[currentIndex].audio.act2, action);
     }
     
     private void AddButtonListener(AlphabetButton button)
     {
         button.onClick += (value) =>
         {
-            button.button.interactable = false;
-            selected.Add(button);
+            ButtonClickMotion(button);
+        };
+    }
 
-            if (selected.Count == 2)
+    private void ButtonClickMotion(AlphabetButton button)
+    {
+        button.button.interactable = false;
+        selected.Add(button);
+
+        if (selected.Count == 2)
+        {
+            if (selected[0].value == alphabets[currentIndex] && selected[1].value == alphabets[currentIndex] && selected[0].type != selected[1].type)
             {
-                if (selected[0].value == alphabets[currentIndex] &&  selected[1].value == alphabets[currentIndex] && selected[0].type != selected[1].type)
+                guideFinger.gameObject.SetActive(false);
+                var clip = words[currentIndex].act;
+                answerImage.Show(words[currentIndex].sprite);
+                audioPlayer.Play(clip, () =>
                 {
-                    var clip = words[currentIndex].act;
-                    answerImage.Show(words[currentIndex].sprite);
-                    audioPlayer.Play(clip,()=>
+                    answerImage.gameObject.SetActive(false);
+                    eventSystem.enabled = true;
+                    currentIndex += 1;
+                    if (CheckOver())
                     {
-                        answerImage.gameObject.SetActive(false);
-                        eventSystem.enabled = true;
-                        currentIndex += 1;
-                        if (CheckOver())
-                        {
+                        if(!isGuide)
                             ShowResult();
-                        }
                         else
                         {
+                            isGuide = false;
+                            guideFinger.gameObject.SetActive(false);
+
+                            var target = upper.Union(lower).ToArray();
+                            foreach (var item in target)
+                                item.button.interactable = true;
+
+                            currentIndex = 0;
+                            selected.Clear();
                             PlayWord();
                         }
-                    });
-                }
-                else
-                {
-                    selected[0].button.interactable = true;
-                    selected[1].button.interactable = true;
-                }
-                selected.Clear();
+                    }
+                    else
+                    {
+                        PlayWord(() => isNext = true);
+                    }
+                });
             }
             else
             {
-                audioPlayer.Play(1f,clipClick);
+                selected[0].button.interactable = true;
+                selected[1].button.interactable = true;
             }
-
-        };
+            selected.Clear();
+        }
+        else
+        {
+            audioPlayer.Play(1f, clipClick, () => isNext = true );
+        }
     }
 }
