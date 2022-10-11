676dd1be13c4c25c5e7e9f79d9c924ad716c957a
yungwon <kwony1537@naver.com>
2022-10-11 18:48:31 +0900

    5단계 108번 Guide 구현

--- Assets/Scripts/Contents/Level_5/JT_PL5_108/JT_PL5_108.cs
+++ Assets/Scripts/Contents/Level_5/JT_PL5_108/JT_PL5_108.cs
@@ -15,10 +15,54 @@ public class JT_PL5_108 : SingleAnswerContents<Question5_108, DigraphsWordsData>
 
     public DoubleClickButton[] buttonQuestions;
     public ImageButton buttonPhanics;
+    private List<DigraphsWordsData> buttonDatas = new List<DigraphsWordsData>();
     public Sprite spritePop;
     public AudioClip clipPop;
 
     private DigraphsWordsData current;
+    bool isNext = false;
+    protected override IEnumerator ShowGuidnceRoutine()
+    {
+        yield return new WaitForEndOfFrame();
+
+
+        guideFinger.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+
+
+        for (int j = 0; j < QuestionCount; j++)
+        {
+            var correctIndex = 0;
+            isNext = false;
+            for (int i = 0; i < buttonDatas.Count(); i++)
+            {
+                Debug.Log(buttonDatas[i].key);
+                if (buttonDatas[i].key == currentQuestion.correct.key)
+                {
+                    correctIndex = i;
+                    break;
+                }
+            }
+            guideFinger.gameObject.SetActive(true);
+
+            guideFinger.DoMoveCorrect(buttonQuestions[correctIndex].transform.position, () =>
+            {
+                guideFinger.DoClick(() =>
+                {
+                    buttonQuestions[correctIndex].isOn = true;
+                    audioPlayer.Play(buttonDatas[correctIndex].clip);
+
+                    guideFinger.DoClick(() =>
+                    {
+                        CorrectClickMotion(buttonQuestions[correctIndex], buttonDatas[correctIndex]);
+
+                        guideFinger.gameObject.SetActive(false); ;
+                    });
+                });
+            });
+
+            while (!isNext) yield return null;
+        }
+    }
 
     protected override List<Question5_108> MakeQuestion()
     {
@@ -45,11 +89,13 @@ public class JT_PL5_108 : SingleAnswerContents<Question5_108, DigraphsWordsData>
 
     protected override void ShowQuestion(Question5_108 question)
     {
+        buttonDatas.Clear();
         for (int i = 0; i < buttonQuestions.Length; i++)
         {
             var data = question.totalQuestion[i];
             buttonQuestions[i].isOn = false;
             buttonQuestions[i].sprite = data.sprite;
+            buttonDatas.Add(data);
             AddDoubleClickListener(buttonQuestions[i], data);
         }
         audioPlayer.Play(question.correct.clip);
@@ -91,32 +137,38 @@ public class JT_PL5_108 : SingleAnswerContents<Question5_108, DigraphsWordsData>
 
         button.onClick.AddListener(() =>
         {
-            if (currentQuestion.correct == data)
-            {
-                button.button.targetGraphic.gameObject.GetComponent<Image>().sprite = spritePop;
-                button.image.gameObject.SetActive(false);
-                audioPlayer.Play(1f, clipPop);
-                var tween = button.GetComponent<RectTransform>().DOScale(1.5f, .25f);
-                tween.SetLoops(2, LoopType.Yoyo);
-                tween.SetEase(Ease.Linear);
-                tween.onComplete += () =>
-                {
-                    AddAnswer(data);
-                    if (!CheckOver())
-                        button.image.gameObject.SetActive(true);
-                };
-                tween.Play();
-            }
-            else
-            {
-                for (int i = 0; i < buttonQuestions.Length; i++)
-                {
-                    buttonQuestions[i].isOn = false;
-                }
-                ResetQuestion();
-            }
+            CorrectClickMotion(button, data);
         });
     }
+    private void CorrectClickMotion(DoubleClickButton button, DigraphsWordsData data)
+    {
+        if (currentQuestion.correct == data)
+        {
+            button.button.targetGraphic.gameObject.GetComponent<Image>().sprite = spritePop;
+            button.image.gameObject.SetActive(false);
+            audioPlayer.Play(1f, clipPop);
+            var tween = button.GetComponent<RectTransform>().DOScale(1.5f, .25f);
+            tween.SetLoops(2, LoopType.Yoyo);
+            tween.SetEase(Ease.Linear);
+            tween.onComplete += () =>
+            {
+                isNext = true;
+                AddAnswer(data);
+                if (!CheckOver())
+                    button.image.gameObject.SetActive(true);
+            };
+            tween.Play();
+        }
+        else
+        {
+            for (int i = 0; i < buttonQuestions.Length; i++)
+            {
+                buttonQuestions[i].isOn = false;
+            }
+            ResetQuestion();
+        }
+    }
+
     protected override void ShowResult()
     {
         base.ShowResult();
