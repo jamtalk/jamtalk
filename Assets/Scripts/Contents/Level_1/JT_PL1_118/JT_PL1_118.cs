27477c1b8a4b2083454d8ceff4a3e51772001b3b
yungwon <kwony1537@naver.com>
2022-10-12 10:21:14 +0900

    1단계 118번 Guide 구현

--- Assets/Scripts/Contents/Level_1/JT_PL1_118/JT_PL1_118.cs
+++ Assets/Scripts/Contents/Level_1/JT_PL1_118/JT_PL1_118.cs
@@ -13,6 +13,49 @@ public class JT_PL1_118 : SingleAnswerContents<Question118, AlphabetWordsData>
     private int alphabetCount = 2;
 
     protected override eContents contents => eContents.JT_PL1_118;
+
+    bool isPlayed = false;
+
+    protected override IEnumerator ShowGuidnceRoutine()
+    {
+        yield return base.ShowGuidnceRoutine();
+
+        for( int i = 0; i < QuestionCount; i ++)
+        {
+            while (!isPlayed) yield return null;
+            isPlayed = false;
+
+            var isNext = false;
+            var target = buttonsAnswer.Where(x => x.name == questions[i].correct.key).First();
+            guideFinger.gameObject.SetActive(true);
+            Debug.Log(questions[i].correct.key);
+            guideFinger.DoMoveCorrect(target.transform.position, () =>
+            {
+                guideFinger.DoClick(() =>
+                {
+                    guideFinger.gameObject.SetActive(false);
+                    guideFinger.transform.position = Vector3.zero;
+                    ButtonClickMotion(target, questions[i].correct);
+                    isNext = true;
+                });
+            });
+
+            while (!isNext) yield return null;
+            isNext = false;
+        }
+    }
+
+    private void ButtonClickMotion(ImageButton button, AlphabetWordsData data)
+    {
+        button.button.interactable = false;
+        audioPlayer.Play(data.clip, () =>
+        {
+            AddAnswer(data);
+            if (!CheckOver())
+                button.button.interactable = true;
+        });
+    }
+
     private void AddButtonListener(ImageButton button, AlphabetWordsData data)
     {
         button.button.onClick.RemoveAllListeners();
@@ -20,13 +63,7 @@ public class JT_PL1_118 : SingleAnswerContents<Question118, AlphabetWordsData>
         {
             if(data == currentQuestion.correct)
             {
-                button.button.interactable = false;
-                audioPlayer.Play(data.clip, () =>
-                 {
-                     AddAnswer(data);
-                     if (!CheckOver())
-                         button.button.interactable = true;
-                 });
+                ButtonClickMotion(button, data);
             }
         });
     }
@@ -58,13 +95,14 @@ public class JT_PL1_118 : SingleAnswerContents<Question118, AlphabetWordsData>
 
     protected override void ShowQuestion(Question118 question)
     {
-        for(int i = 0;i < buttonsAnswer.Length; i++)
+        for (int i = 0; i < buttonsAnswer.Length; i++)
         {
             AddButtonListener(buttonsAnswer[i], question.totalQuestion[i]);
             buttonsAnswer[i].sprite = question.totalQuestion[i].sprite;
+            buttonsAnswer[i].name = question.totalQuestion[i].key;
         }
 
-        audioPlayer.Play(question.correct.act);
+        audioPlayer.Play(question.correct.act, () => isPlayed = true);
         buttonQuestion.onClick.RemoveAllListeners();
         buttonQuestion.onClick.AddListener(() => audioPlayer.Play(question.correct.act));
 
