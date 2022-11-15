a3612fe1195ef9a50a22d913e8076e902092ca04
yungwon <kwony1537@naver.com>
2022-11-15 19:54:14 +0900

    Match Image 정답 확인 로직 수정

--- Assets/Scripts/Contents/BaseMatchImage.cs
+++ Assets/Scripts/Contents/BaseMatchImage.cs
@@ -9,6 +9,16 @@ using Random = UnityEngine.Random;
 public abstract class BaseMatchImage<T> : BaseContents
     where T : ResourceWordsElement
 {
+    protected abstract void GetWords();
+    protected abstract void PlayAudio(ResourceWordsElement word);
+    protected override eContents contents => throw new System.NotImplementedException();
+    protected override int GetTotalScore() => drops.Length;
+    protected override bool CheckOver() => questionCounts == index;
+    protected virtual bool DropCompleted() => !drops.Select(x => x.isConnected).Contains(false);
+    protected virtual int questionCounts => 1;
+    protected int index = 0;
+
+
     public CanvasScaler scaler;
     public DropSpaceShip_107[] drops;
     public DragKnob_107[] drags;
@@ -18,46 +28,35 @@ public abstract class BaseMatchImage<T> : BaseContents
     {
         yield return base.ShowGuidnceRoutine();
 
-        //for(int i = 0; i < drops.Length; i++)
-        //{
-            var dropTarget = drops.Where(x => !x.isConnected).OrderBy(x => Random.Range(0, 100)).First();
-            var dragTarget = drags.Where(x => x.data.key == dropTarget.data.key).First();
 
-            guideFinger.transform.localScale = new Vector3(1f, 1f, 1f);
-            guideFinger.DoMove(dropTarget.pointKnob.transform.position, () =>
+        var dropTarget = drops.Where(x => !x.isConnected).OrderBy(x => Random.Range(0, 100)).First();
+        var dragTarget = drags.Where(x => x.data.key == dropTarget.data.key).First();
+
+        guideFinger.transform.localScale = new Vector3(1f, 1f, 1f);
+        guideFinger.DoMove(dropTarget.pointKnob.transform.position, () =>
+        {
+            guideFinger.DoPress(() =>
             {
-                guideFinger.DoPress(() =>
-                {
-                    PlayAudio(dropTarget.data);
-                    dropTarget.SetGuideLine(1f ,dragTarget);
+                PlayAudio(dropTarget.data);
+                dropTarget.SetGuideLine(1f, dragTarget);
 
-                    guideFinger.DoMove(1f, dragTarget.pointKnob.transform.position, () =>
-                    {
-                        dropTarget.SetGuideCover(dragTarget);
-                        guideFinger.gameObject.SetActive(false);
-                        isNext = true;
-                    });
+                guideFinger.DoMove(1f, dragTarget.pointKnob.transform.position, () =>
+                {
+                    dropTarget.SetGuideCover(dragTarget);
+                    guideFinger.gameObject.SetActive(false);
+                    isNext = true;
                 });
             });
+        });
 
-            while (!isNext) yield return null;
-            isNext = false;
-        //}
+        while (!isNext) yield return null;
+        isNext = false;
     }
     protected override void Awake()
     {
         base.Awake();
         GetWords();
     }
-
-    protected abstract void GetWords();
-
-
-    protected abstract void PlayAudio(ResourceWordsElement word);
-
-    protected override eContents contents => throw new System.NotImplementedException();
-    protected override int GetTotalScore() => drops.Length;
-    protected override bool CheckOver() => !drops.Select(x => x.isConnected).Contains(false);
 
     protected virtual void SetElement(T[] item)
     {
@@ -89,19 +88,26 @@ public abstract class BaseMatchImage<T> : BaseContents
 
     protected virtual void onDrop()
     {
-        if (CheckOver())
-            ShowResult();
+        if(DropCompleted())
+        {
+            index++;
+
+            if (CheckOver())
+                ShowResult();
+            else
+                ResetElement();
+        }
         else
+        {
             audioPlayer.Play(1f, GameManager.Instance.GetClipCorrectEffect(), () =>
             {
                 if (isGuide) EndGuidnce();
             });
+        }
     }
 
-    protected override void EndGuidnce()
+    protected virtual void ResetElement()
     {
-        base.EndGuidnce();
-
         foreach (var item in drags)
             item.Reset();
 
@@ -112,5 +118,12 @@ public abstract class BaseMatchImage<T> : BaseContents
         }
 
         GetWords();
+    }
+
+    protected override void EndGuidnce()
+    {
+        base.EndGuidnce();
+
+        ResetElement();
     }
 }
