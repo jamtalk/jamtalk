48d34d8c32fd008d615249a93b8399edf29bfa3f
yungwon <kwony1537@naver.com>
2022-11-16 14:50:54 +0900

    Pooling Object 추가

---
+++ Assets/Scripts/Pooling.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class Pooling<T> : MonoBehaviour
+    where T : PoolingElement
+{
+    public List<PoolingElement> pool = new List<PoolingElement>();
+    public virtual T GetObject(T orizinal, Transform parent) 
+    {
+        var targets = pool.Where(x => !x.gameObject.activeSelf);
+        T result;
+        if (targets.Count() > 0)
+        {
+            result = targets.First().GetComponent<T>();
+            result.transform.SetParent(parent);
+        }
+        else
+        {
+            result = Instantiate(orizinal, parent);
+            result.name = orizinal.name;
+            pool.Add(result);
+        }
+        result.gameObject.SetActive(true);
+        return result;
+    }
+}
