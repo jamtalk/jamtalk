cfa8f8ea31386ef4f18310f6f8d8d0c1520718de
yungwon <kwony1537@naver.com>
2022-11-22 16:09:30 +0900

    Scene Exit Button Prefab 구현

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using GJGameLibrary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    public Button button;
+
+    private void Awake()
+    {
+        button.onClick.AddListener(() =>
+        {
+            GJSceneLoader.Instance.LoadScene(eSceneName.AD_008);
+        });
+    }
+}
