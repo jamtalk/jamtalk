573845fbd8c72893d3650ef25dcb16ed1b8bf093
yungwon <kwony1537@naver.com>
2022-11-29 20:40:31 +0900

    AD_010 ( 캐릭터 선택 ) 신 1차 구현

---
+++ Assets/Scripts/UI/AD_010/CharactorElement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Spine.Unity;
+using UnityEngine.UI;
+
+public class CharactorElement : MonoBehaviour
+{
+    public SkeletonGraphic charactor;
+    public Image plat;
+    public RectTransform rt;
+    //[HideInInspector]
+    public int index;
+
+    public void Init(RectTransform rect, int index)
+    {
+        StartCoroutine(InitCoroutine(rect, index));
+    }
+    public IEnumerator InitCoroutine(RectTransform rect, int index)
+    {
+        this.index = index;
+        transform.position = rect.position;
+        yield return new WaitForEndOfFrame();
+        //int colorA = index < 0 ? 255 - (index * -1) : 255 - index;
+        //Color color = charactor.color;
+        //color.a = colorA;
+        //charactor.color = color;
+        //plat.color = color;
+    }
+
+    public void Move(RectTransform rect, int index, TweenCallback callback = null)
+    {
+        this.index = index;
+        int colorA = index < 0 ? 255 - index * -1 : 255 - index;
+
+        Sequence seq = DOTween.Sequence();
+
+        Tween moveTween = transform.DOMove(rect.position, 1f);
+        //Tween trnspTween = charactor.DOFade(colorA, 1f);
+        //Tween trnspPlatTween = plat.DOFade(colorA, 1f);
+
+        seq.Insert(0, moveTween);
+        //seq.Insert(0, trnspTween);
+        //seq.Insert(0, trnspPlatTween);
+        seq.onComplete += callback;
+        seq.Play();
+    }
+}
---
+++ Assets/Scripts/UI/AD_010/SelectController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Spine.Unity;
+using System.Linq;
+
+public class SelectController : MonoBehaviour
+{
+    public Button leftButton;
+    public Button rightButton;
+    public Button selectButton;
+    public Text text;
+    public RectTransform leftRt;
+    public RectTransform rightRt;
+    public CharactorElement[] elements;
+    public RectTransform[] rects;
+
+    private void Awake()
+    {
+        Init();
+
+
+        leftButton.onClick.AddListener(() => OnClickListener(false));
+        rightButton.onClick.AddListener(() => OnClickListener(true));
+    }
+
+    public void OnClickListener(bool isRight)
+    {
+        var index = 1;
+        var count = 0;
+
+        if (isRight)
+        {
+            index = 1;
+            count = elements.Where(x => x.index >= rects.Length).Count();
+        }
+        else
+        {
+            index = -1;
+            count = elements.Where(x => x.index < 0).Count();
+        }
+
+        if (elements.Length - count == 3)
+            return;
+
+        foreach (var item in elements)
+        {
+            var temp = index + item.index;
+            if (temp == 2) text.text = item.name;
+            if (temp >= rects.Length)
+                item.Move(rightRt, temp);
+            else if (temp < 0)
+                item.Move(leftRt, temp);
+            else
+                item.Move(rects[temp], temp);
+        }
+    }
+
+
+    public void Init()
+    {
+        for (int i = 0; i < elements.Length; i++)
+        {
+            if (i < rects.Length)
+                elements[i].Init(rects[i], i);
+            else
+                elements[i].Init(rightRt, i);
+            elements[i].name = elements[i].charactor.name;
+            if (i == 2) text.text = elements[i].name;
+        }
+    }
+}
