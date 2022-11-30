72f8c93a14fdf554842686a894a544ba700ab106
yungwon <kwony1537@naver.com>
2022-11-30 22:42:09 +0900

    AD_004 1차 구현

---
+++ Assets/Scripts/UI/AD_013/SelectChild.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectChild : MonoBehaviour
+{
+    public TMP_InputField inputName;
+    public TMP_InputField inputBirth;
+    public Toggle maleToggle;
+    public Toggle femaleToggle;
+    public Toggle termsToggle;
+    public Button termsMoreButton;
+    public Button confirmButton;
+    // button prefab
+
+    private void Awake()
+    {
+    }
+}
