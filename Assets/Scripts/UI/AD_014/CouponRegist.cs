8bbb6224511bdf6737f940346b1d5bada6fc5349
yungwon <kwony1537@naver.com>
2022-12-01 18:07:46 +0900

    AD_014 구현 / 적용

---
+++ Assets/Scripts/UI/AD_014/CouponRegist.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CouponRegist : MonoBehaviour
+{
+    public TMP_InputField couponInput;
+    public Button registButton;
+    public Button exitButton;
+    public Action<string> registAction;
+
+    private void OnEnable()
+    {
+        couponInput.text = string.Empty;
+    }
+
+    private void Awake()
+    {
+        registButton.onClick.AddListener(() => RegistAction());
+        exitButton.onClick.AddListener(() => gameObject.SetActive(false));
+    }
+
+    private void RegistAction()
+    {
+        if (couponInput.text == string.Empty)
+            registAction?.Invoke(couponInput.text);
+    }
+}
---
+++ Assets/Scripts/UI/AD_014/MembershpPage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MembershpPage : MonoBehaviour
+{
+    public Button couponButton;
+    public Button monthButton;
+    public Button yearthButton;
+    public CouponRegist couponRegist;
+
+    private void Awake()
+    {
+        couponButton.onClick.AddListener(() => couponRegist.gameObject.SetActive(true));
+    }
+}
