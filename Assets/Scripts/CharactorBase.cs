1fd619d6a37707a2674e9cf5959944161a921f19
yungwon <kwony1537@naver.com>
2022-12-04 16:00:21 +0900

    Spine Animation 이름 버그 수정

---
+++ Assets/Scripts/CharactorBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+
+public class CharactorBase : MonoBehaviour
+{
+    public SkeletonGraphic charactor; 
+}
