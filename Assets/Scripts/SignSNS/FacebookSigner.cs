8900f3c63cce7c72cc238c1f8c1b1951a74d3e05
GameJE <xx9582@naver.com>
2023-02-02 12:03:03 +0900

    파이어베이스 로그인 구현 완료

--- Assets/Scripts/SignSNS/FacebookSigner.cs
+++ Assets/Scripts/SignSNS/FacebookSigner.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Facebook.Unity;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 public class FacebookSigner : BaseSigner
 {
     protected override eProvider provider => eProvider.facebook;
@@ -19,44 +23,64 @@ public class FacebookSigner : BaseSigner
     }
     private IEnumerator SigninFB()
     {
+        Debug.Log("페이스북 로그인 프로세스 시작");
         if (!FB.IsInitialized)
+        {
+            Debug.Log("페이스북 이니셜라이징");
             FB.Init(InitCallback,OnHideUnity);
+        }
 
         while (!FB.IsInitialized) { yield return null; }
+        Debug.Log("로그아웃 진행");
+        FB.LogOut();
+        Debug.Log("로그아웃 완료");
         bool success=false;
         bool recived = false;
-        AccessToken token = null;
-        Profile profile = null;
-        FB.LogInWithReadPermissions(new string[] { "name", "email", "id" }, (result)=>
+        string uid = string.Empty;
+        string email = string.Empty;
+        string name = string.Empty;
+        Debug.Log("페이스북 로그인 요청");
+        FB.LogInWithReadPermissions(new string[] {
+            "email",
+            "public_profile",
+            "user_friends"
+        }, (result) =>
         {
+            Debug.LogFormat("로그인 요청 결과\n", result);
             success = FB.IsLoggedIn;
+            Debug.LogFormat("로그인 성공여부 : {0}", FB.IsLoggedIn);
             if (FB.IsLoggedIn)
             {
-                token = AccessToken.CurrentAccessToken;
-                Debug.Log("UID: " + token.UserId);
-                Debug.Log("Access Token: " + token.TokenString);
-                Debug.LogFormat("받아온 토큰 상태\n{0}", token.ToJson());
-                foreach (string perm in token.Permissions)
+                var token = AccessToken.CurrentAccessToken;
+                uid = token.UserId;
+                Debug.Log("받아온 UID: " + token.UserId);
+                Debug.Log("받아온 Access Token: " + token.TokenString);
+                Debug.LogFormat("토큰의 권한 목록 : {0}", string.Join(", ", token.Permissions));
+                Debug.LogFormat("받아온 토큰 상태\n{0}", JObject.Parse(token.ToJson()));
+                FB.API("/me?fields=name,picture", HttpMethod.GET, (callback)=>
                 {
-                    // 접근 권한이 있는 항목들
-                    Debug.Log("perm: " + perm);
-                }
-                FB.CurrentProfile((profile_result)=>
-                {
-                    Debug.LogFormat("profile 결과\n{0}", profile_result.RawResult);
-                    profile = profile_result.CurrentProfile;
+                    Debug.Log("이름 요청 완료");
+                    Debug.Log(JObject.Parse(callback.RawResult));
+                    var dic = callback.ResultDictionary;
+                    name = dic["name"].ToString();
+                    if (dic.ContainsKey("email"))
+                        email = dic["email"].ToString();
+                    else
+                        email = string.Empty;
+                    recived = true;
                 });
             }
             else
             {
                 Debug.Log("User cancelled login");
+                recived = true;
             }
-            recived = true;
         });
 
         while (!recived) { yield return null; }
 
-        ExistSNS(eProvider.facebook, token.UserId, profile.Email, profile.Name);
+        if (success)
+            ExistSNS(eProvider.facebook, uid, email, name);
     }
     private void InitCallback()
     {
@@ -67,27 +91,6 @@ public class FacebookSigner : BaseSigner
         else
         {
             Debug.Log("Failed to Initialize the Facebook SDK");
-        }
-    }
-
-    private void AuthCallback(ILoginResult result)
-    {
-        if (FB.IsLoggedIn)
-        {
-            var token = AccessToken.CurrentAccessToken;
-
-            Debug.Log("UID: " + token.UserId);
-            Debug.Log("Access Token: " + token.TokenString);
-            Debug.LogFormat("받아온 토큰 상태\n{0}", token.ToJson());
-            foreach (string perm in token.Permissions)
-            {
-                // 접근 권한이 있는 항목들
-                Debug.Log("perm: " + perm);
-            }
-        }
-        else
-        {
-            Debug.Log("User cancelled login");
         }
     }
 
