1d5e8b786e564da635d9ef66f5697d995dc7bb69
GameJaneE <xx9582@naver.com>
2023-05-31 13:58:14 +0900

    자녀 칭찬 목록, 다음 진행해야할 컨텐츠 목록 구현

--- Assets/Scripts/UI/AD_010_1/ViewerPage.cs
+++ Assets/Scripts/UI/AD_010_1/ViewerPage.cs
@@ -1,14 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
 
 public class ViewerPage : UserInfoScene
 {
+    [Header("학습 시간")]
     public Text textRegistDate;
     public Text textToday;
     public Text textTotalDay;
+
+    [Space(10)]
+    [Header("남은 학습목록")]
+    public Image leftElement;
+
+    [Space(10)]
+    [Header("칭찬목록")]
+    public GameObject commendation;
     public override void Init()
     {
         base.Init();
@@ -24,8 +35,31 @@ public class ViewerPage : UserInfoScene
     private void GetLearningDeatil()
     {
         // 남은 학습 목록 생성 
+        var min = UserDataManager.Instance.CurrentChild.level * 100;
+        var max = (UserDataManager.Instance.CurrentChild.level+1) * 100;
+        var contents = Enum.GetNames(typeof(eContents))
+            .Select(x => (eContents)Enum.Parse(typeof(eContents), x))
+            .Where(x => (int)x >= min)
+            .Where(x => (int)x < max)
+            .Take(5)
+            .ToArray();
+        for(int i = 0;i < contents.Length; i++)
+        {
+            var item = Instantiate(leftElement, leftElement.transform.parent);
+            SetImage(item, contents[i]);
+            item.gameObject.SetActive(true);
+        }
 
         // 칭찬 목록 생성
+    }
+
+    private void SetImage(Image image, eContents contents)
+    {
+        Addressables.LoadAssetAsync<Sprite>(contents.ToString()).Completed += (op) =>
+        {
+            image.sprite = op.Result;
+            image.preserveAspect = true;
+        };
     }
 
     private string GetDate(DateTime time)
